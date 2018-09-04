8ca8afd4eced1a2c2d4db60d86e6af9728c6401b
Juhan Houang <jhouang@ea.com>
2018-09-03 21:11:01 -0700

    deploy prod build try with deploy-url updated app-root

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ angular.module('lesleys-books', [
   // Now set up the states
   $stateProvider
     .state('current-year-bookshelf', {
-      url: "/",
+      url: "/bookshelf",
       template: "<current-year-bookshelf></current-year-bookshelf>"
     })
     .state('all-time-favourites', {
